8ef809c7577ead1c6cb85a785b9f34217af648ad
Saeed Shahrivari <saeed.shahrivari@gmail.com>
2022-05-26 20:23:11 +0430

    Unify configuration of pubsub.

--- src/main/java/com/github/shahrivari/redipper/redis/RedisSubscriber.kt
+++ src/main/java/com/github/shahrivari/redipper/redis/RedisSubscriber.kt
@@ -2,12 +2,11 @@ package com.github.shahrivari.redipper.redis
 
 import com.github.shahrivari.redipper.config.RedisConfig
 
-class RedisSubscriber(private val host: String, private val port: Int = 6379) {
-    private lateinit var redis: RedisPubSubDao
+class RedisSubscriber(config: RedisConfig) {
+    private var redis: RedisPubSubDao = RedisPubSubDao.create(config)
     private val callbackMap = mutableMapOf<String, (ByteArray) -> Unit>()
 
     init {
-        createPublisherRedisDao()
         redis.onMessage { channel, message ->
             val callback = checkNotNull(callbackMap[String(channel)]) {
                 "No callback registered for channel $channel in RedisSubscriber."
@@ -16,12 +15,6 @@ class RedisSubscriber(private val host: String, private val port: Int = 6379) {
         }
     }
 
-    private fun createPublisherRedisDao() {
-        redis = RedisPubSubDao.create(RedisConfig().apply {
-            ipList = setOf(host)
-            port = this@RedisSubscriber.port
-        })
-    }
 
     fun subscribe(topic: String, callback: (ByteArray) -> Unit) {
         redis.subscribe(topic.toByteArray())
